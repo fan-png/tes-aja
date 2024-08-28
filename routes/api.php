0261119f72a2a79860ef0eccaf174cd01fac1c08
fan-png <fajarandhika654@gmail.com>
2024-08-28 15:18:46 +0700

    #

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\ArticleController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::apiResource('articles', ArticleController::class);
+
+
